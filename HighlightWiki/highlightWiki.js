6832eeb56e1ea2c3e41a1774064751d7bf899e3f
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-09-24 12:55:26 +0530

    Added background.html. Able to get selected text but not the context menu.

--- HighlightWiki/highlightWiki.js
+++ HighlightWiki/highlightWiki.js
@@ -2,6 +2,20 @@
  * Part of the HighlightWiki Project
  * Author : Rajat Khanduja
  */
+
+var appendHighlightTags = function (text) {
+  return "<span style='background-color:yellow;'>" + text + "</span>"
+};
+
+$(document).mouseup(function(event)
+{
+    var sel = window.getSelection().toString();
+    if(sel.length)
+        chrome.extension.sendRequest({'message':'setText','data': sel},function(response){})
+})
+
 $(function(){
   alert ("working");
-})
+});
+
+
